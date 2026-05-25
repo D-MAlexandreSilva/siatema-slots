486bcdfbb8d4d9c8cf6a9f4c3421717e2e1dcd45
D-MAlexandreSilva <alexandresdmaria454@gmail.com>
2026-05-25 18:55:13 -0300

    deploy render

--- app.py
+++ app.py
@@ -96,6 +96,5 @@ def lista():
 
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True)                                                                                           
+    app.run(host="0.0.0.0", port=5000)                                                                                     
